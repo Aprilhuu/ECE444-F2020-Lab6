6f61820037bd44e03b0a0993aa45e7757c64eead
Aprilhuu <38861292+Aprilhuu@users.noreply.github.com>
2020-10-30 12:50:38 -0400

    Adding login required with tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -78,6 +78,22 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
+
+
+def test_login_required(client):
+    """Test login required functionality"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    assert data["message"] == 'Please log in.'
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
